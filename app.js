68df7d55c04f7a2d9066bdaf597e4e9ea9c52734
Yousef Qandeel <yqandil7@gmail.com>
2022-02-12 22:24:50 +0200

    Add JS to my project

---
+++ app.js
@@ -0,0 +1,39 @@
+var gender = "";
+var def = "";
+var userName = prompt("Enter Your Name");
+var userGender = prompt("Enter Your Gender");
+var userAge = prompt("Enter Your Age");
+
+if (userGender === "Male" || userGender === "Female") {
+    gender = userGender;
+    if (gender == "Female") {
+        def = "Ms.";
+    }
+    else if (gender == "Male") {
+        def = "Mr.";
+    }
+}
+else {
+    alert("Gender Will Be Only ('Male' Or 'Femal') Please Rewrite It");
+    userGender = prompt("Enter Your Gender");
+}
+
+var age = Number(userAge);
+if (age <= 0) {
+    alert("*Warning, Your Age Is Equal Or Less Than Zero");
+}
+var conText = "Do You Want To Skip Wellcoming Message"
+var con = confirm(conText);
+
+switch (con) {
+    case true:
+        alert("You Skiped Welcoming Message");
+        break;
+
+    case false:
+        alert(`Welcome ${def} ${userName}`);
+        break;
+
+    default:
+        alert("Welcome To Our Website");
+}
